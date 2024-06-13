4cd15b11660dc47cd568baa0bde4cf59480d420f
Ayan <ayan2572006@gmail.com>
2024-06-13 06:33:32 +0530

    fix formData not Get Issue

--- controller/userController.js
+++ controller/userController.js
@@ -2,11 +2,9 @@ import asyncHandler from "../middleware/asyncHandler.js";
 import ErrorHandler from "../utils/errorHandler.js";
 
 const registerUser = asyncHandler(async (req, res, next) => {
-  const { name, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-  if (!name || !email || !password)
-    return next(new ErrorHandler("Please enter all fields", 400));
-
+  console.log(username);
   res.status(200).json({ message: "Register User" });
 });
 export { registerUser };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { registerUser } from "../controller/userController.js";
-
+import fileUpload from "../middleware/multer.js";
 const router = express.Router();
-router.route("/").post(registerUser);
+router.route("/").post(fileUpload, registerUser);
 export default router;
